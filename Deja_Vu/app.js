94e8a59f24d85be3fbb5a073fd934ed7e635731a
David Howard <hotcheesesoup6@aol.com>
2022-02-16 11:38:33 -0600

    Not finished

--- Deja_Vu/app.js
+++ Deja_Vu/app.js
@@ -1,22 +1,51 @@
-console.log("Hello World!");
+console.log("Deja Vu");
 
-let userGuess = prompt(`Type in a number! You can guess between ${minGuess} and ${maxGuess}.`);
+let userName = prompt("Name please.");
 
+let numberSet = prompt("Choose a set of numbers to guess between: 1 through 10, 11 through 20, 21 through 30. ");
 
-let maxGuess = 100;
-let rand = 3;
-let minGuess = 1;
+let minGuess;
+let maxGuess;
+let rand;
+
+switch (numberSet) {
+    case "1 through 10":
+        rand = 3;
+        minGuess = 1;
+        maxGuess = 10;
+        break;
+    case "11 through 20":
+        rand = 14;
+        minGuess = 11;
+        maxGuess = 20;
+        break;
+    case "21 through 30":
+        rand = 28;
+        minGuess = 21;
+        maxGuess = 30;
+        break;
+    default:
+        rand = 7;
+        minGuess = 1;
+        maxGuess = 10;
+}
+
+let userGuess = prompt(
+  `${userName}, Type in a number! You can guess between ${minGuess} and ${maxGuess}.`
+);
 
 if (userGuess < minGuess || userGuess > maxGuess) {
-    alert(`You gave me a number that was not between ${minGuess} and ${maxGuess}. Try Again!`
+  alert(
+    `${userName}, You gave me a number that was not between ${minGuess} and ${maxGuess}. Try Again!`
     );
 } else {
-
-    if (rand > userGuess) {
-        alert("You guessed too low!");
-    } else if (rand < userGuess) {
-        alert("You guessed too high!");
-    } else {
-        alert("Congrats your did it! Dosn't it feel like a real win?!");
-    }
+  if (rand > userGuess) {
+    alert("You guessed too low!");
+  } else if (rand < userGuess) {
+    alert("You guessed too high!");
+  } else {
+    alert(`Congrats your did it! Dosn't it feel like a real win ${userName}?!`);
+  }
 }
+
+
