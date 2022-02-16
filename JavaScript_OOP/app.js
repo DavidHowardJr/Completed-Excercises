d66f98222e941866f4c5d68da13f370d60693872
David Howard <hotcheesesoup6@aol.com>
2022-02-16 10:21:38 -0600

    Finished OOP Exercise

--- JavaScript_OOP/app.js
+++ JavaScript_OOP/app.js
@@ -1,4 +1,54 @@
-console.log("Hello World!\n==========\n");
+console.log("JS OOP Exercise");
 
-// Exercise 1 Section
-console.log("EXERCISE 1:\n==========\n");
+class Person {
+  constructor(name, numPets, residence, hobbies) {
+    this.name = name;
+    this.numPets = numPets;
+    this.residence = residence;
+    this.hobbies = hobbies;
+  }
+
+  info() {
+    console.log(
+      `${this.name} has ${this.numPets} pet(s), lives in ${
+        this.residence
+      }, and enjoys ${this.hobbies.join(" and ")}.`
+    );
+  }
+
+  greet() {
+    console.log("Hello!");
+  }
+}
+
+class Coder extends Person {
+  constructor(name, numPets, residence, hobbies) {
+    super(name, numPets, residence, hobbies);
+
+    this.occupation = "Full stack Web Developer";
+  }
+
+  greet() {
+    console.log("Hi TrueCoder!");
+  }
+}
+
+let ben = new Person("Ben", 1, "Hoover", [
+  "coding",
+  "running",
+  "ping pong",
+  "reading",
+]);
+
+let seth = new Person("Seth", 0, "Warrior", ["skate boarding", "video games"]);
+
+ben.info(); // Ben has 1 pet(s), lives in Hoover, and enjoys coding and running and ping pong and reading.
+ben.greet(); // Hello
+
+seth.info();
+seth.greet();
+
+let tanner = new Coder("Tanner", 0, "Trussville", ["video games", "coding"]);
+
+tanner.info();
+tanner.greet();
