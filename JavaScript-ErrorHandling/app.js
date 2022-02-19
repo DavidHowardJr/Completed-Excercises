b173501e18e131f195e4a8a3a5d5ee29ced68c1b
David Howard <hotcheesesoup6@aol.com>
2022-02-19 15:53:52 -0600

    JS ErrorHandling

---
+++ JavaScript-ErrorHandling/app.js
@@ -0,0 +1,34 @@
+"use strict";
+let userMessage;
+let repeatMessageThisManyTimes;
+
+class UserInputError extends Error {}
+
+function takeUserInput() {
+  userMessage = prompt("Please enter a message!");
+  //console.log(userMessage);
+  do {
+    try {
+      repeatMessageThisManyTimes = Number(
+        prompt("How many times would you like your message to be repeated?")
+      );
+      if (!repeatMessageThisManyTimes) {
+        throw new UserInputError("Not a Number.");
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  } while (!repeatMessageThisManyTimes);
+}
+
+takeUserInput();
+
+function repeatMessage() {
+  for (let i = 0; i < repeatMessageThisManyTimes; i++) {
+    console.log(userMessage + " " + i);
+  }
+}
+
+repeatMessage();
+// what the hell
+
